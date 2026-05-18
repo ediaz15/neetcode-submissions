34dc499028a3268e2db8e9dae316fe5747e8fb69
Erick Diaz <ediazluc@gmail.com>
2026-05-18 15:54:55 -0500

    Add: buy-and-sell-crypto - submission-1

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-1.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int maxProfit = 0;
+        int i = 0;
+        int j = 0;
+        while(j < prices.length-1){
+            if(prices[i] > prices[j]){
+                //profit is negative
+                i = j;
+            }
+            j++;
+            maxProfit = Math.max(prices[j] - prices[i], maxProfit);
+        }
+
+        return maxProfit;
+    }
+}
